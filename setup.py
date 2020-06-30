4c28298ae4f06d0848950152c8faf9d7d5224275
Karina Tiemi <karinat@take.net>
2020-06-30 14:48:48 -0300

    added setup template

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="package_name",
+    version="0.0.1",
+    author="my_name",
+    author_email="my_email",
+    description="My short description",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="my_github_repository_project_link"
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
